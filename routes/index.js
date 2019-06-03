4a1cebc4a5a6398f6e31d4acd2ea33126abf6b89
Eyobeil  Fesseha <44642778+eyobeil@users.noreply.github.com>
2019-06-02 22:46:54 -0500

    Delete index.js

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (request, response) => {
-	response.send("You've got nothing to loose!");
-});
-
-module.exports = router;
